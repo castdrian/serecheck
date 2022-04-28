a23b3217156aa3f7b95164a18b8d6bf06b680dc5
adrifcastr <adrifcastr@gmail.com>
2022-04-28 08:47:02 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from ntpath import join
+from halo import Halo
+from html_to_json import convert
+from urllib.request import urlopen
+
+spinner = Halo(text='Fetching Serebii.net')
+spinner.start()
+
+page = urlopen('https://serebii.net/index2.shtml')
+html_bytes = page.read()
+html = html_bytes.decode('utf8', 'ignore')
+
+json = convert(html)
+news = json['html'][0]['body'][0]['div'][0]['div'][1]['main'][0]['div'][0]['div'][1]['p'][1]['_values']
+spinner.stop()
+
+print(f'Latest news entry:\n\n{join(news[0])}')
